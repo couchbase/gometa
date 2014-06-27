533511a2b79d8872da5e54bdcf619a5c694b14b3
jliang00 <john.liang@couchbase.com>
2014-06-27 16:29:59 -0700

    fix compile error

--- src/repository/commit_log.go
+++ src/repository/commit_log.go
@@ -12,6 +12,7 @@ import (
 
 type CommitLog struct {
 	repo    *Repository
+	factory *message.ConcreteMsgFactory
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -22,60 +23,60 @@ type CommitLog struct {
 // Create a new commit log
 //
 func NewCommitLog(repo *Repository) *CommitLog {
-	return &CommitLog{repo : repo}
+	return &CommitLog{repo : repo, factory : message.NewConcreteMsgFactory()}
 }
 
 //
 // Add Entry to commit log 
 //
-func (r *CommitLog) Log(txid Txnid, op string, key string, content []byte) error {
+func (r *CommitLog) Log(txid common.Txnid, op common.OpCode, key string, content []byte) error {
 
     k := createKey(txid)
-    msg := message.CreateLogEntry(op, key, content)
+    msg := r.factory.CreateLogEntry(uint32(op), key, content)
    	data, err := common.Marshall(msg) 
    	if err != nil {
    		return err
    	}
     
-	return r.Set(k, data)	
+	return r.repo.Set(k, data)	
 }
 
 //
 // Retrieve entry from commit log
 //
-func (r *CommitLog) Get(txid Txnid) (string, string, []byte, error) {
+func (r *CommitLog) Get(txid common.Txnid) (common.OpCode, string, []byte, error) {
 
     k := createKey(txid) 
-    data, err := r.Get(k) 
+    data, err := r.repo.Get(k) 
     if err != nil {
-    	return nil, nil, nil, err
+    	return common.OPCODE_INVALID, "", nil, err
     }
     
-   	packet, err := common.UnMarshaller(data[8:])
+   	packet, err := common.UnMarshall(data[8:])
    	if err != nil {
-   		return err
+   		return common.OPCODE_INVALID, "", nil, err
    	}
    	entry := packet.(message.LogEntry)
-   	return entry.GetOpCode(), entry.GetKey(), entry.GetContent()
+   	return entry.GetOpCode(), entry.GetKey(), entry.GetContent(), nil
 }
 
 //
 // Delete from commit log 
 //
-func (r *CommitLog) Delete(txid Txnid) error {
+func (r *CommitLog) Delete(txid common.Txnid) error {
 
     k := createKey(txid)
-    return r.Delete(k) 
+    return r.repo.Delete(k) 
 }
 
 ////////////////////////////////////////////////////////////////////////////
 // Private Function 
 /////////////////////////////////////////////////////////////////////////////
 
-func createKey(txid Txnid) (string, error) {
+func createKey(txid common.Txnid) (string) {
 	
     buf := []byte(common.PREFIX_COMMIT_LOG_PATH)
     buf = strconv.AppendInt(buf, int64(txid), 10)
 
-   	return string(k), nil 
+   	return string(buf)
 }
