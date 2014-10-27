d870753e46c84afbcb6713be027dada5d0260055
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2014-10-27 14:07:51 -0700

    Run gofmt on repository/repo.go.
    Change-Id: Id0d4928999f41ade50a94aecd5bd115478d5cce9
    Reviewed-on: http://review.couchbase.org/42498
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Reviewed-by: John Liang <johliang00@gmail.com>
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- repository/repo.go
+++ repository/repo.go
@@ -34,7 +34,7 @@ func OpenRepository() (*Repository, error) {
 func OpenRepositoryWithName(name string) (*Repository, error) {
 
 	config := fdb.DefaultConfig()
-	config.SetBufferCacheSize(1024*1024)
+	config.SetBufferCacheSize(1024 * 1024)
 	db, err := fdb.Open(name, config)
 	if err != nil {
 		return nil, err
